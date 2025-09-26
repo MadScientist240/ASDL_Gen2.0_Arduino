c9f77f95586e83515a5f5371ba29627703f193c1
MadScientist <1xlr8ghast@gmail.com>
2025-09-25 23:44:00 -0400

    Added config structure
    Config structure is used for enabling sensors and configuring sample rate.

--- Gen-2-Spoke/Gen-2-Spoke.ino
+++ Gen-2-Spoke/Gen-2-Spoke.ino
@@ -10,14 +10,10 @@
 #include <Adafruit_Sensor.h>
 #include "Adafruit_BMP3XX.h"
 
-#define SCL_PIN 7
-#define SDA_PIN 6
-#define SEALEVELPRESSURE_HPA (1013.25)  // FIXME: Adjust properly
-
 // Sensors
 Adafruit_BMP3XX bmp388;
 
-// Sensor Data Structure
+// Sensor Structures
 typedef struct {
   float temperature;    // °C
   float pressure;       // kPa
@@ -27,17 +23,36 @@ typedef struct {
   float battery_level;  // %
 }data_struct;
 
+typedef struct {
+  int sample_rate;      // seconds
+  bool bmp_enabled;
+  bool scd_enabled;
+}config_struct;
+
 data_struct data;
+config_struct config = {5, true, true};
+
+#define SCL_PIN 7
+#define SDA_PIN 6
+#define SEALEVELPRESSURE_HPA (1013.25)  // FIXME: Adjust properly
+
+
 
 void setup() {
   Serial.begin(115200);
   Wire.begin(SDA_PIN, SCL_PIN);
 
   // BMP388 Init:
-  bmp_init();
+  if(config.bmp_enabled){
+    bmp_init();
+  } else {
+    Serial.println("BMP388 is disabled in config");
+  }
+  
 }
 
 void loop() {
+  delay(config.sample_rate * 1000);
   bmp_read();
 }
 
@@ -57,7 +72,7 @@ bool bmp_init(){
 
 bool bmp_read(){
   if(!bmp388.performReading()){
-    Serial.print("BMP388 Reading Failed");
+    Serial.println("BMP388 Reading Failed");
     return false;
   } else {
     Serial.println("========== BMP388 ==========");
